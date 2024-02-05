1ba24c3e2ca2b20f6dfd6cb1b7eb556d5f2807d8
chatany <75946423+chatany@users.noreply.github.com>
2024-02-05 17:25:45 +0530

    palindrom checker

--- array.js
+++ array.js
@@ -1,7 +1,29 @@
-Write a function that returns the string "something" joined with a space " " and the given argument a.
 
-function str(a) {
-    return "something"+" "+a
+let a=document.getElementById("input").value
+function check() {
+    if(a==""){
+         alert("plesase Enter Text")
+        
+    }else{
+        let arr=a.toLowerCase()
+        let arr2=arr.split("")
+        let arr1=arr.split("").reverse()
+        console.log(arr2);
+        console.log(arr1);
+        localStorage.setItem("name",JSON.stringify(a))
+     
+        for(i=0;i<arr1.length;i++){
+        if(arr2[i]==arr1[i]){
+            document.getElementById("result").innerHTML= a+" is a palindrome"
+        }
+        else if(arr2[i]!=arr1[i]){
+            document.getElementById("result").innerHTML= a+` is not a palindrome number`
+        }
+    }
+}
+}
+   
+function input() {
+    a=document.getElementById("input").value
 
 }
-//this is a
